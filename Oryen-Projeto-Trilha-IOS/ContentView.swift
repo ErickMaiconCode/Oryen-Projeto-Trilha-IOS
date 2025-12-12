26ad14e8679912cb16ca07d0f98a1100866f6bbd
Erick <erick.lima195@outlook.com>
2025-12-12 15:53:51 -0300

    Initial Commit

---
+++ Oryen-Projeto-Trilha-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Oryen-Projeto-Trilha-IOS
+//
+//  Created by Erick Maicon Lima de Almeida on 12/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
